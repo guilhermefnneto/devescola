dec40921a2f11a5796b0d177d95a215fb0e6f303
guilhermefnneto <guilherme.fn.neto@outlook.com>
2019-03-19 11:48:03 -0300

    Implementação da página de alunos. Inclusão da biblioteca do font-awesome.

--- devescola/src/main/webapp/js/aluno.js
+++ devescola/src/main/webapp/js/aluno.js
@@ -4,44 +4,157 @@ var aluno = function() {
 	"use strict";
 
 
-	var tabAlunos = $('#tab-alunos');
+	var $tabAlunos       = $('#tabAlunos'),
+		$modalFormAluno  = $('#modalFormAluno'),
+	    $formAluno       = $('#formAluno'),
+	    modo             = null;
 
 
 	_init();
 
 
+	/** Inicializa a página, carregando os alunos previamente cadastrados. */
 	function _init () {
-		alunoApi.obterTodosOsAlunos(_preencherTabelaDeAlunos);
+		$tabAlunos.find('thead th div a').click(function() {
+			novo();
+		});
+
+		$modalFormAluno.find('#actSalvar').click(function() {
+			salvar();
+		});
+
+		alunoApi.obterTodosOsAlunos(preencherTabelaDeAlunos, processarErro);
 	}
 
 
-	function _preencherTabelaDeAlunos(alunos) {
-		var $tbody = tabAlunos.find('tbody');
+	/** Preenche a grid que representa os alunos. */
+	function preencherTabelaDeAlunos(alunos) {
+		var $tbody = $tabAlunos.find('tbody');
 
 		if (!Array.isArray(alunos)) {
 			alunos = [alunos];
 		}
 
-		$.each(alunos, function(i, item) {
+		$.each(alunos, function(i, aluno) {
 			var $tr = $('<tr>').append(
-				$('<td>').text(item.ra).css("text-align","right"),
-				$('<td>').text(item.nome),
-				$('<td>').text(item.sexo).css("text-align","center"),
-				$('<td>').text(item.dataNascimento).css("text-align","center").attr("data-formatter", "dataFormatter")
+				$('<td>').text(aluno.ra).css("text-align","right"),
+				$('<td>').text(aluno.nome),
+				$('<td>').text(aluno.sexo).css("text-align","center"),
+				$('<td>').text(_dataFormatter(aluno.dataNascimento)).css("text-align","center").attr("data-formatter", "dataFormatter")
 			);
+
+			var $iconEdit = $('<i>').addClass("fa fa-pencil").css("cursor", "pointer");
+			var $aEdit = $('<a>').attr("title", "Editar").append($iconEdit);
+			$aEdit.click(function(){
+				editar(aluno.ra);
+			});
+
+			var $iconRemove = $('<i>').addClass("fa fa-trash").css("cursor", "pointer");
+			var $aRemove = $('<a>').attr("title", "Remover").append($iconRemove);
+			$aRemove.click(function() {
+				remover(aluno.ra);
+			});
+
+			var $divAct = $('<div>').css("display", "inline").css("margin-right", "5px");
+			$divAct.append($aEdit);
+			$divAct.append($aRemove);
+
+			var $tdAct = $('<td>').css("text-align", "center");
+			$tdAct.append($divAct);
+
+			$tr.append($tdAct);
 
 			$tbody.append($tr);
 		});
 
-		$tbody.find('td[data-formatter="dataFormatter"]').each(function(i, item) {
-			var $item = $(item);
-			var value = _dataFormatter($item.text());
-			$item.text(value);
-		});
 	}
 
+	/** Formata uma data para a máscara DD/MM/YYYY. */
 	function _dataFormatter(value) {
 		return moment(value).format('DD/MM/YYYY');
+	}
+
+	/** Abre o formulário para o registro de um novo aluno. */
+	function novo() {
+		limparForm();
+
+		modo = 'novo';
+		
+		$modalFormAluno.modal();
+	}
+
+	/** Carrega o formulário com os dados do aluno a ser atualizado, identificado pelo seu ra. */
+	function editar(ra) {
+		modo = 'edição';
+
+		alunoApi.obterAlunoByRa(ra, preencherFormulario, processarErro);
+	}
+
+	/** Solicita a remoção de um aluno do quadro de alunos. */
+	function remover(ra) {
+		console.log('remove: ', ra);
+	}
+
+
+	/** Limpa o formulário. */
+	function limparForm() {
+		$formAluno[0].reset();
+	}
+
+
+	/** Salva o aluno editado ou o novo aluno informado. */
+	function salvar() {
+		var metodo = modo === 'edição' ?'PUT' :'POST';
+
+		var aluno = {
+			ra: $formAluno.find('input[name="ra"]').val(),
+			nome: $formAluno.find('input[name="nome"]').val(),
+			sexo: $formAluno.find('input[name="sexo"]:checked').val(),
+			dataNascimento: $formAluno.find('input[name="dataNascimento"]').val(),
+		};
+
+		if (modo === 'edição') {
+			alunoApi.atualizarAluno(aluno, _processarGravacaoDoFormulario);
+		} else {
+			alunoApi.incluirAluno(aluno, _processarGravacaoDoFormulario);
+		}
+
+	}
+
+
+	/** Preenche o formulário com os dados do aluno passado como argumento. */
+	function preencherFormulario(aluno) {
+		$formAluno.find('input[name="ra"]').val(aluno.ra);
+		$formAluno.find('input[name="nome"]').val(aluno.nome);
+		$formAluno.find('input[name="sexo"][value="'+aluno.sexo+'"]').prop("checked", true);
+		$formAluno.find('input[name="dataNascimento"]').val(aluno.dataNascimento);
+
+		$modalFormAluno.modal();
+	}
+
+
+	/** Processa a resposta  */
+	function _processarGravacaoDoFormulario(response) {
+		console.log('dados refletidos: ',response);
+	}
+
+	/** Emite uma mensagem de sucesso. */
+	function processarSucesso(data) {
+		console.log('Sucesso na operação!');
+		console.log('sucesso: ', data);
+	}
+
+	/** Emite uma mensagem de erro. */
+	function processarErro(error) {
+		console.log('Erro na operação!');
+		console.log('erro: ', error);
+	}
+
+
+	return {
+		editar: editar,
+		remover: remover,
+		novo: novo
 	}
 
 };
--- devescola/src/main/webapp/js/alunoapi.js
+++ devescola/src/main/webapp/js/alunoapi.js
@@ -10,32 +10,82 @@ var alunoApi = {
 
 
 	/** Aciona o serviço que recupera todos os alunos. */
-	obterTodosOsAlunos: function obterTodosOsAlunos(_processarDados) {
-		$.getJSON(this.host, function(response) {
-			_processarDados(response);
+	obterTodosOsAlunos: function obterTodosOsAlunos(_processarDados, _processarErro) {
+		$.ajax({
+			url: this.host,
+			method: 'GET',
+			data: null,
+			success: function(response) {
+				_processarDados(response);
+			},
+			error: function(response) {
+				_processarErro(response);
+			}
 		});
 	},
 
  	/** Aciona o serviço que recupera um aluno identificado pelo seu RA. */
-	obterAlunoByRa: function obterAlunoByRa(ra, _processarDados) {
-		$.getJSON(this.host+"/"+ra, function(response) {
-			_processarDados(response);
+	obterAlunoByRa: function obterAlunoByRa(ra, _processarDados, _processarErro) {
+		$.ajax({
+			url: this.host + '/' + ra,
+			method: 'GET',
+			data: null,
+			success: function(response) {
+				_processarDados(response);
+			},
+			error: function(response) {
+				_processarErro(response);
+			}
 		});
 	},
 
 	/** Aciona o serviço que inclui um aluno. */
 	incluirAluno: function incluirAluno(aluno, _processarResposta) {
-
+		$.ajax({
+			url: this.host,
+			method: 'POST',
+			data: aluno,
+			success: function(response) {
+				_processarResposta(response);
+			},
+			error: function(response) {
+				_processarResposta(response);
+			}
+		});
 	},
 
 	/** Aciona o serviço que atualiza os dados do aluno */
 	atualizarAluno: function atualizarAluno(aluno, _processarResposta) {
+		console.log('ra/atualizar: ', ra, aluno);
 
+
+		$.ajax({
+			url: this.host + "/" + aluno.ra,
+			method: 'PUT',
+			contentType: "application/json; charset=utf-8",
+			data: aluno,
+			success: function(response) {
+				_processarResposta(response);
+			},
+			error: function(response) {
+				_processarResposta(response);
+			}
+		});
 	},
 
 	/** Aciona o serviço que remove o aluno identificado pelo seu RA. */
 	removerAluno: function removerAluno(ra, _processarResposta) {
-
+		$.ajax({
+			url: this.host + "/" + ra,
+			method: 'DELETE',
+			data: null,
+			success: function(response) {
+				_processarResposta(response);
+			},
+			error: function(response) {
+				_processarResposta(response);
+			}
+		});
 	}
 
 }
